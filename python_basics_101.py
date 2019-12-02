afb97faf4483c50243448ed9ea0f1b46e145ff1d
Sam Watson <swatson@spartaglobal.com>
2019-12-02 12:15:42 +0000

    First Commit

---
+++ python_basics_101.py
@@ -0,0 +1,24 @@
+# Variables
+## It's like a box
+## Has a name and has stuff inside
+## Can re-use the box
+
+# Assignment of variable
+## Simple = to e.g:
+example_variable = 'hello eng 48'
+
+# Reassignment of variable
+## Just rename e.g:
+example_variable = 48
+
+# Calling variable: Print
+print(example_variable)
+
+# Print statement --> outputs to console
+
+# Print with type() to find out property of variable
+example_variable2 = '48'
+print(type(example_variable))
+print(type(example_variable2))
+
+
