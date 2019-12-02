21be7b8425df23c0e6f20885d741f5a9f07bc697
Sam Watson <SWatson@spartaglobal.com>
2019-12-02 17:05:29 +0000

    First Commit

---
+++ python_basics_dict.py
@@ -0,0 +1,12 @@
+# Dictionary {}
+# AKA --> Hashes or object in JavaScript
+## Works exactly like a dictionary
+## Has key and value pairs: Key = word, Value = meaning of word
+### Dict = {'Key' : 'Value'}
+# Syntax
+my_dictionary = {'Tissues': 'Disposable piece of absorbent paper. used especially as a handkerchief or for cleaning the skin',
+                 'Baseball Bat': 'Bat made of wood to play baseball'}
+# Accessing or Re-assigning is with the key inside the []
+my_dictionary['Baseball Bat'] = 'Really, you are looking up two seperate words'
+print(my_dictionary['Baseball Bat'])
+
