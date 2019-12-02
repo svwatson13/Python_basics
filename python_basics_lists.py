b80b9bf377c3e16e0fee89e3b2b69a42a42806b4
Sam Watson <SWatson@spartaglobal.com>
2019-12-02 16:49:43 +0000

    First Commit

---
+++ python_basics_lists.py
@@ -0,0 +1,56 @@
+# Lists
+## Ordered by indexes
+## Lists are also known as arrays or objects in JavaScript
+
+# Syntax
+## Define lists using []
+## Seperate items using ,
+
+## Creating list with items
+crazy_x_landlords = ['Sr. Julio', 'Jane', 'Alfred', 'Marksons']
+print(crazy_x_landlords)
+print(type(crazy_x_landlords))
+
+# How to access 1 record in the list: <list>[] - index starts at 0
+## minus starts from the end so -1 is the last in the list
+print(crazy_x_landlords[2])
+print(crazy_x_landlords[0])
+print(crazy_x_landlords[-1])
+
+# New list of places to live
+places_to_live = ['California', 'Rio De Janeiro', 'Melbourne', 'Manchester', 'Singapore']
+# Reassign Index
+places_to_live[3] = 'Hawaii'
+print(places_to_live)
+
+# Method .append()
+places_to_live.append('Amsterdam')
+print(places_to_live)
+
+# Method .insert(<Index>, <item>): Insert new item
+places_to_live.insert(4, 'Prague')
+print(places_to_live)
+
+# Method .pop(<Index>): Delete specific item
+places_to_live.pop(3)
+print(places_to_live)
+
+# List slicing
+## Used to manage lists
+### Printing from index to end of list - Not including index specified
+print(places_to_live[3:])
+### Printing from index to beginning of list -Not including index specified
+print(places_to_live[:3])
+### Printing from specified index to second specified index (Not inlusive of last but does include first)
+print(places_to_live[0:3])
+### Skip slicing - first number where it starts : middle number is limit : last number is increments
+### Start:Stop:Increment
+print(places_to_live[0:6:2]) # Start at 0, go to 6 in increments of 2
+print(places_to_live[::-1]) # Backwards
+
+# Tuples ()
+## Immutable lists
+## syntax
+mortal_enemies = ('Mario', 'Sailormoon', 'MOON CAKE', 'Jerry', 'Berry')
+print(type(mortal_enemies))
+
